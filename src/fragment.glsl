e99eadb6883abec5da542e9f5ec0ed82a5bbcdd5
Juuso Tapaninen <juuso.tapaninen@gmail.com>
2024-09-22 19:06:10 +0300

    feat: draw 2D circle
    2D circle SDF is fairly easy to understand, displaying anything 3D
    might be a no go in time. I think raymarching and 3D SDFs is the
    way to go for 3D in the fragment shader, but still not 100% sure.
    
    Leaving visuals at this for now, need to focus on web audio side
    and making it possible to trigger the visuals plus audio based on
    user interaction so that audio can play properly

--- src/fragment.glsl
+++ src/fragment.glsl
@@ -6,6 +6,27 @@ uniform float time;
 
 out vec4 outColor;
 
+// https://iquilezles.org/articles/distfunctions2d/
+float circle(in vec2 point, in float radius) {
+  return length(point) - radius;
+}
+
 void main() {
-  outColor = vec4(fract(gl_FragCoord.xy / res), fract(time), 1);
+  vec2 coord = (2.0f * gl_FragCoord.xy - res) / res.y;
+
+  float distance = circle(coord, 0.5f);
+
+  vec3 outsideColor = vec3(0.0f, 0.56f, 0.85f);
+  vec3 insideColor = vec3(0.66f, 0.45f, 0.07f);
+
+  // Circle
+  vec3 color = distance > 0.0f ? outsideColor : insideColor;
+  // Shadow
+  color *= 1.0f - exp(-6.0f * abs(distance));
+  // Waves
+  color *= 0.8f + 0.2f * cos(150.0f * distance - 18.0f * time);
+  // Border
+  color = mix(color, vec3(1.0f), 1.0f - smoothstep(0.0f, 0.01f, abs(distance)));
+
+  outColor = vec4(color, 1);
 }
